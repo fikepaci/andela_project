ad5766acbe2a6fb8d151a6db2fab54a87059147b
irakoze pacifique <pacifiquemachine@gmail.com>
2019-09-05 22:26:35 -0700

    [bg #168334543] change app.js file

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
+import swaggerui from 'swagger-ui-express';
 import userRoutes from './routes/user.route';
 import sessionroutes from './routes/session.routes';
 import reviewroutes from './routes/review.routes';
+import swaggerdocument from '../swagger.json';
 
 dotenv.config();
 
@@ -16,5 +18,6 @@ app.use('/api/v1', userRoutes);
 app.use('/api/v1', sessionroutes);
 app.use('/api/v1', reviewroutes);
 
+app.use('/docs', swaggerui.serve, swaggerui.setup(swaggerdocument));
 
 export default app;
